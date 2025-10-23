21ee358e87b202f1c4951490fde2fcb6bab004e7
sqisitoo <92672259+sqisitoo@users.noreply.github.com>
2025-10-23 12:39:59 +0200

    Create empty extract.py module

---
+++ include/air-pollution/tasks/extract.py
@@ -0,0 +1,10 @@
+
+
+def extract_data_from_api():
+    """
+    Extract air poluution data from an external API.
+
+    Returns:
+        dict: A dictionary containing the extracted air pollution data.
+    """
+    
