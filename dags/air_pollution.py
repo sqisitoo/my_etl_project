a4e196363868b292b4fc97b4d46af620d27fcbfc
sqisitoo <92672259+sqisitoo@users.noreply.github.com>
2025-11-02 17:13:18 +0100

    Update dag with extract part

--- dags/air_pollution.py
+++ dags/air_pollution.py
@@ -2,6 +2,13 @@ from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sdk import dag, task
 from datetime import datetime, timedelta
+from include.air_pollution.tasks.extract import get_cities_config
+from airflow.models.variable import Variable
+import pendulum
+
+cities_to_process = get_cities_config()
+
+
 
 @dag(
     dag_id="air_pollution_dag",
@@ -9,8 +16,8 @@ from datetime import datetime, timedelta
     schedule='@daily',
     catchup=False,
     default_args={
-        "retries": 3,
-        "retry_delay": timedelta(minutes=2)
+        "retries": 2,
+        "retry_delay": timedelta(minutes=1)
     }
 )
 def air_pollution_dag():
@@ -21,9 +28,50 @@ def air_pollution_dag():
         task_id="check_api_availability",
         http_conn_id="weathermap_api",
         endpoint="/data/2.5/air_pollution/history",
-        response_check=lambda response: response.status_code in [200, 401, 403]
+        response_check=lambda response: response.status_code in [200, 401],
+        extra_options={'check_response': False}
     )
 
-    @task 
-    def extract_data():
-        pass
+    @task
+    def get_current_timestamps_range():
+        start_ts = int(Variable.get('air_pollution_last_ts'))
+        end_ts = int(pendulum.now(tz='UTC').timestamp())
+
+        time_range = {'start_ts': start_ts, 'end_ts': end_ts}
+        return time_range
+
+    @task
+    def extract_data(city_info: dict, time_range: dict, logical_date):
+        from include.air_pollution.tasks.extract import extract_air_pollution_data
+
+        print(f"GOT time_range: {time_range}")
+
+        return extract_air_pollution_data(
+            city=city_info['city'],
+            lat=city_info['lat'],
+            lon=city_info['lon'],
+            start_ts=time_range['start_ts'],
+            end_ts=time_range['end_ts'],
+            logical_date=logical_date
+            )
+    
+    @task
+    def update_last_timestamp(time_range: dict):
+        new_ts = time_range['end_ts']
+        Variable.set('air_pollution_last_ts', str(new_ts))
+        print(f"Last timestamp updated to: {new_ts}")
+
+    time_range_task_output = get_current_timestamps_range()
+
+    extract_tasks_group = extract_data.partial(time_range=time_range_task_output).expand(city_info=cities_to_process)
+
+    update_timestamp_task = update_last_timestamp(
+        time_range=time_range_task_output
+    )
+
+    start >> check_api >> time_range_task_output >> extract_tasks_group >> update_timestamp_task
+
+air_pollution_dag()
+
+
+
