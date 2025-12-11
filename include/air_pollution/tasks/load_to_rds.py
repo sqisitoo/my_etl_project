71d2b9abcc20cdb9a1e7dcf292bd52c2e4f88ff1
sqisitoo <92672259+sqisitoo@users.noreply.github.com>
2025-12-11 16:48:09 +0100

    Refactor load_to_rds to use load_parquet_from_s3 and remove commented code

--- include/air_pollution/tasks/load_to_rds.py
+++ include/air_pollution/tasks/load_to_rds.py
@@ -8,8 +8,7 @@ import pyarrow.parquet as pq
 import io
 from sqlalchemy import create_engine
 
-
-BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
+from include.air_pollution.utils.s3_common import load_parquet_from_s3
 
 DB_USER= os.getenv("DB_USER")
 DB_PASSWORD=os.getenv("DB_PASSWORD")
@@ -18,14 +17,6 @@ DB_PORT=os.getenv("DB_PORT")
 DB_NAME=os.getenv("DB_NAME")
 
 def load_to_rds(path_to_transformed_data: str):
-
-    # s3 = boto3.client('s3')
-    # response = s3.get_object(Bucket=BUCKET_NAME,Key=path_to_transformed_data)
-
-    # table = pq.read_table(io.BytesIO(response['Body'].read()))
-
-    # df = table.to_pandas()
-    from include.air_pollution.utils.s3_common import load_parquet_from_s3
     df = load_parquet_from_s3(path_to_transformed_data)
 
     connection_string = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
