7b00041866d8f9167298d8018241d50ccd92164d
sqisitoo <92672259+sqisitoo@users.noreply.github.com>
2026-02-09 07:55:00 +0100

    feat: add backend configuration for Terraform state management

--- terraform/network.tf
+++ terraform/network.tf
@@ -68,3 +68,54 @@ resource "aws_route_table_association" "public_2" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "airflow" {
+  allocation_id = aws_eip.nat.id
+  subnet_id =  aws_subnet.public_1.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.airflow.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.airflow.id
+  }
+
+  tags = {
+    Name = "airflow_private_rt"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id = aws_vpc.airflow.id
+
+  cidr_block = "10.0.3.0/24"
+
+  availability_zone = "eu-north-1a"
+
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id = aws_vpc.airflow.id
+
+  cidr_block = "10.0.4.0/24"
+
+  availability_zone = "eu-north-1b"
+
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
